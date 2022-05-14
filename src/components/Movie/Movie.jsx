c3838947a53d3dfbc4ed76315a9806fcb0d77fbf
Meir Sedlis <meirsedlis@gmail.com>
2022-05-14 15:47:01 -0500

    create details component

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
+
 function Movie({movie}){
+    const history = useHistory();
+
     return (
         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} onClick={getDetails}/>
                         </div>
     )
 }
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,7 @@
+function MovieDetails(){
+    return(
+        <h1>I DO EXIST!</h1>
+    )
+}
+
+export default MovieDetails
