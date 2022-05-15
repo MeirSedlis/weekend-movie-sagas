d6976d44abff8cae7dcff46c22058b49e6ff24ae
Meir Sedlis <meirsedlis@gmail.com>
2022-05-15 10:41:44 -0500

    write GET route on server

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+router.get('/:id', (req, res) => {
+  const queryText = `SELECT movies.id, movies.title, genres.name FROM movies JOIN movies_genres ON movies.id = movies_genres.movie_id JOIN genres ON genres.id = movies_genres.genre_id WHERE movies.id=$1;`;
+  pool.query(queryText, [req.params.id])
+    .then((result) => { res.send(result.rows); })
+    .catch((err) => {
+      console.log('Error in GET genres', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
